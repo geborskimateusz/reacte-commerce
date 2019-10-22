3bb347c3ac68f1f182e894f99c7e684c4c52b01b
carrothead <Loczek11>
2019-10-22 23:22:19 +0200

    added withRouter navigation to main-item.component.jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import './App.css';
-import {HomePage} from './pages/homepage/homepage.component';
+import { HomePage } from './pages/homepage/homepage.component';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
-   <div>
-     <HomePage/>
-   </div>
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path={'/shop/hats'} component={HatsPage} />
+      </Switch>
+    </div>
   );
 }
 
