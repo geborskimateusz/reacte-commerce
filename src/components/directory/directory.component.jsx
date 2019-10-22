b772cdf29c89f1f68a26ff0ce0b31198e6036a9e
carrothead <Loczek11>
2019-10-22 16:35:50 +0200

    added directory component

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./directory.styles.scss";
+import { DIRECTORY_SECTIONS } from "./directory.data";
+import MenuItem from "../menu-item/menu-item.component";
+
+class Directory extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sections: [...DIRECTORY_SECTIONS]
+    };
+  }
+
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({title, imageUrl, id, size}) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Directory;
