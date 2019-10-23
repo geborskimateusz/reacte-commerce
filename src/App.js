3637e551d3c429c9ee5a3b2e89bde1d4fa50dba2
carrothead <Loczek11>
2019-10-23 12:27:16 +0200

    added signin component prototype

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { HomePage } from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import  Header  from './components/header/header.component';
+import SignPage from './pages/sign/sign.component';
 import {
   BrowserRouter as Router,
   Switch,
@@ -16,7 +17,8 @@ function App() {
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />>
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/sign' component={SignPage} />
       </Switch>
     </div>
   );
---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import "./sign-in.styles.scss";
+
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  handleChange = e => {
+    const { value, name } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    console.log(this.state);
+
+    this.setState({ email: "", password: "" });
+  };
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <input
+            name="email"
+            type="email"
+            value={this.state.email}
+            required
+            onChange={this.handleChange}
+          />
+          <label>Email</label>
+
+          <input
+            name="password"
+            type="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+            required
+          />
+          <label>Password</label>
+          <input type="submit" value="Sign in" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
