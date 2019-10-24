2835f8b2040c7c4c37de7d1bc2bfa4090ad07aee
carrothead <Loczek11>
2019-10-24 19:21:35 +0200

    added add to cart reducer

--- src/redux/ui/cart/cart-reducer.jsx
+++ src/redux/ui/cart/cart-reducer.jsx
@@ -1,17 +1,23 @@
 import CartActionTypes from "./cart-types";
 
 const initialState = {
-  hidden: true
+  hidden: true,
+  cartItems: []
 };
 
 const cartReducer = (state = initialState, action) => {
   switch (action.type) {
     case CartActionTypes.TOOGLE_CART_HIDDEN:
-        console.log('toogle')
       return {
         ...state,
         hidden: !state.hidden
       };
+    
+      case CartActionTypes.ADD_ITEM:
+        return {
+          ...state,
+          cartItems: [...state.cartItems, action.payload ]
+        }
 
     default:
       return {
