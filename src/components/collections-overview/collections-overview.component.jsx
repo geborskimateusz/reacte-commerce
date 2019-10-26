2286cc9c0b9a8fae938363c75938babda87cab6f
carrothead <Loczek11>
2019-10-26 16:28:16 +0200

    migrated collections overview into separate component

---
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { connect } from "react-redux";
+import "./collections-overview.styles.scss";
+import { createStructuredSelector } from "reselect";
+import CollectionPreview from "../collection-preview/collection-preview.component";
+import {selectShopCollections} from '../../redux/shop/shop.selector.js';
+
+const CollectionsOverview = ({ collections }) => (
+  <div className="collections-overview">
+    {collections.map(({ id, ...collectionProps }) => (
+      <CollectionPreview key={id} {...collectionProps} />
+    ))}
+  </div>
+);
+
+const mapStateToProps = createStructuredSelector({
+    collections: selectShopCollections
+  })
+  
+  export default connect(mapStateToProps)(CollectionsOverview);
