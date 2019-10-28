56cb192741f9e654413db2825c6993aa1a069912
carrothead <Loczek11>
2019-10-28 19:30:14 +0100

    added form-input styled components

--- src/components/form-input/form-input.jsx
+++ src/components/form-input/form-input.jsx
@@ -1,19 +1,18 @@
 import React from "react";
-import "./form-input.styles.scss";
+// import "./form-input.styles.scss";
+import {
+  GroupContainer,
+  FormInputContainer,
+  FromInputLabelContainer
+} from "./form-input.styles";
 
 const FormInput = ({ handleChange, label, ...otherProps }) => (
-  <div className="group">
-    <input className="form-input" onChange={handleChange} {...otherProps} />
+  <GroupContainer>
+    <FormInputContainer onChange={handleChange} {...otherProps} />
     {label ? (
-      <label
-        className={`${
-          otherProps.value.length ? "shrink" : ""
-        } form-input-label`}
-      >
-        {label}
-      </label>
+      <FromInputLabelContainer {...otherProps}>{label}</FromInputLabelContainer>
     ) : null}
-  </div>
+  </GroupContainer>
 );
 
 export default FormInput;
