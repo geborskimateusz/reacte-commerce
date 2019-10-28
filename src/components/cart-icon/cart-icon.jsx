d349360e80f2ec262f03f99e9719640bbd159a24
carrothead <Loczek11>
2019-10-28 22:10:25 +0100

    added cart-item, cart-icon styled components

--- src/components/cart-icon/cart-icon.jsx
+++ src/components/cart-icon/cart-icon.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
-import "./cart-icon.styles.scss";
 import { connect } from "react-redux";
 import { toogleCartHidden } from "../../redux/cart/cart.actions";
-import {selectCartItemsCount} from "../../redux/cart/cart.selector";
+import { selectCartItemsCount } from "../../redux/cart/cart.selector";
+import {
+  CartIconContainer,
+  ShopingCartIcon,
+  ItemCountSpan
+} from "./cart-icon.styles";
 
 const CartIcon = ({ toogleCartHidden, itemCount }) => (
-  <div className="cart-icon" onClick={toogleCartHidden}>
-    <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">{itemCount}</span>
-  </div>
+  <CartIconContainer onClick={toogleCartHidden}>
+    <ShopingCartIcon />
+    <ItemCountSpan>{itemCount}</ItemCountSpan>
+  </CartIconContainer>
 );
 
 const mapStateToProps = state => ({
