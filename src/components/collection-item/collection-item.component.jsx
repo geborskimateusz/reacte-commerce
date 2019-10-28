828857fcdc862e3e548a9fa7c15bb793b516f07f
carrothead <Loczek11>
2019-10-28 19:54:34 +0100

    added collection-item styled component

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,30 +1,38 @@
 import React from "react";
-import "./collection-item.styles.scss";
-import Button from '../button/button.component';
-import {connect} from 'react-redux';
-import {addItem} from '../../redux/cart/cart.actions.js';
-
+// import "./collection-item.styles.scss";
+import Button from "../button/button.component";
+import { connect } from "react-redux";
+import { addItem } from "../../redux/cart/cart.actions.js";
+import {
+  CollectionItemContainer,
+  CollectionFooterContainer,
+  AddButton,
+  BackgroundImage,
+  NameContainer,
+  PriceContainer
+} from './collection-item.styles';
 const CollectionItem = ({ item, addItem }) => {
-  const {name, price, imageUrl} = item;
+  const { name, price, imageUrl } = item;
 
   return (
-    <div className='collection-item'>
-      <div
-        className='image'
-        style={{
-          backgroundImage: `url(${imageUrl})`
-        }}
-      />
-      <div className='collection-footer'>
-        <span className='name'>{name}</span>
-        <span className='price'>${price}</span>
-      </div>
-      <Button inverted onClick={() => addItem(item)}>ADD TO CART</Button>
-    </div>
-  )};
+    <CollectionItemContainer>
+    <BackgroundImage className='image' imageUrl={imageUrl} />
+    <CollectionFooterContainer>
+      <NameContainer>{name}</NameContainer>
+      <PriceContainer>{price}</PriceContainer>
+    </CollectionFooterContainer>
+    <AddButton onClick={() => addItem(item)} inverted>
+      Add to cart
+    </AddButton>
+  </CollectionItemContainer>
+  );
+};
 
 const mapDispatchToProps = dispatch => ({
   addItem: item => dispatch(addItem(item))
-})
+});
 
-export default connect(null, mapDispatchToProps)(CollectionItem);
+export default connect(
+  null,
+  mapDispatchToProps
+)(CollectionItem);
