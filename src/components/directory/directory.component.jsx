41493b20eb0eb2ef55cad338477c629f8a3f8e59
carrothead <Loczek11>
2019-10-28 19:34:15 +0100

    added directory styled components

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import "./directory.styles.scss";
 import MenuItem from "../menu-item/menu-item.component";
 import {connect} from 'react-redux';
 import { createStructuredSelector } from "reselect";
 import {selectDirectorySections} from '../../redux/directory/directory.selector';
+import {DirectoryMenu} from './directory.styles';
 
 const Directory = ({sections}) => (
-      <div className="directory-menu">
+      <DirectoryMenu>
         {sections.map(({ id, ...sectionProps }) => (
           <MenuItem key={id} {...sectionProps} />
         ))}
-      </div>
+      </DirectoryMenu>
     );
 
 
