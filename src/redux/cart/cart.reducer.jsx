2e99211b808fc5cedbff0e34da3971e51fe8c85f
carrothead <Loczek11>
2019-10-31 15:37:39 +0100

    added clear cart items on sign out sagas

--- src/redux/cart/cart.reducer.jsx
+++ src/redux/cart/cart.reducer.jsx
@@ -34,6 +34,12 @@ const cartReducer = (state = initialState, action) => {
         )
       };
 
+    case CartActionTypes.CLEAR_CART:
+      return {
+        ...state,
+        cartItems: []
+      };
+
     default:
       return state;
   }
