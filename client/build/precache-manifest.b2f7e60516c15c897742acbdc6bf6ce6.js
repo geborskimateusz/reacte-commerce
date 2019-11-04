384e848a676e34ea666724292fc990ef72f10739
carrothead <Loczek11>
2019-11-04 11:33:20 +0100

    added stripe payment, updated .gitignore

---
+++ client/build/precache-manifest.b2f7e60516c15c897742acbdc6bf6ce6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a47b4da2e048f5f35e59ba2dfb18728c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69bedd7b0927653a2097",
+    "url": "/static/css/main.42e7fa50.chunk.css"
+  },
+  {
+    "revision": "a7edd11f514d95e4d7ee",
+    "url": "/static/js/2.b16f8125.chunk.js"
+  },
+  {
+    "revision": "69bedd7b0927653a2097",
+    "url": "/static/js/main.19345b95.chunk.js"
+  },
+  {
+    "revision": "d9c58b67d9ca59578722",
+    "url": "/static/js/runtime-main.f10c1cdd.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
